d99f9454af84efec59733161411bf2ac1dcd844b
Ming <133835846+Mingcode123@users.noreply.github.com>
2026-04-28 19:06:21 +0000

    sync all codespaces outputs

--- MRP.R
+++ MRP.R
@@ -9,7 +9,7 @@ options(mc.cores = detectCores(logical = FALSE))
 set.seed(123)
 
 # Settings
-B = 10
+B = 25
 rep_start = 1
 rep_end = B
 
--- sunMethods.R
+++ sunMethods.R
@@ -7,7 +7,7 @@ options(mc.cores = detectCores(logical = FALSE))
 
 set.seed(123)
 
-B = 10
+B = 25
 rep_start = 1
 rep_end = B
 
